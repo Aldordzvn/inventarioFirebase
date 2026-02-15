d97801c8fa39b3a89d507e720bfdcf7ccd6d0b3e
Aldordzvn <aldordzvn2001@gmail.com>
2026-02-15 16:46:04 -0700

    se añade el header mobile

---
+++ src/app/header/header.component.ts
@@ -0,0 +1,29 @@
+import { Component } from '@angular/core';
+import { FaIconComponent, FontAwesomeModule } from '@fortawesome/angular-fontawesome';
+import { faArrowLeft, faBars, faMoon, faSun } from '@fortawesome/free-solid-svg-icons';
+
+@Component({
+  selector: 'app-header',
+  imports: [FontAwesomeModule],
+  templateUrl: './header.component.html',
+  styleUrl: './header.component.scss'
+})
+export class HeaderComponent {
+  menuIcon = faBars;
+  darkIcon = faMoon;
+  lightIcon = faSun;
+  backIcon = faArrowLeft;
+  openMenuModalBoolean : boolean = false;
+  hasInteracted : boolean = false;
+
+  openModal(){
+    this.openMenuModalBoolean = !this.openMenuModalBoolean;
+    this.hasInteracted = true;
+    
+    if(this.openMenuModalBoolean){
+      document.body.style.overflow = 'hidden';
+    }else{
+      document.body.style.overflow = '';
+    }
+  }
+}
