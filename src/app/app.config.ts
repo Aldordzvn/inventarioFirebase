bc1c01ef0278dc0dda6d38ef5d4dba6773cbfd36
Aldordzvn <aldordzvn2001@gmail.com>
2026-02-16 17:34:05 -0700

    se añade el dark theme

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,15 +1,20 @@
-import { ApplicationConfig, importProvidersFrom, provideZoneChangeDetection } from '@angular/core';
+import { ApplicationConfig, importProvidersFrom, inject, provideAppInitializer, provideZoneChangeDetection } from '@angular/core';
 import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
 import { provideHttpClient } from '@angular/common/http';
 import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
 import {library} from '@fortawesome/fontawesome-svg-core';
+import { ThemeService } from './services/theme/theme.service';
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideZoneChangeDetection({ eventCoalescing: true }),
     provideRouter(routes),
-    provideHttpClient()
+    provideHttpClient(),
+    provideAppInitializer(()=>{
+      const themeService = inject(ThemeService);
+      themeService.initTheme();
+    })
   ]
 };
--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,7 +1,8 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
 import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
 import { faArrowLeft, faBars, faMoon, faSun } from '@fortawesome/free-solid-svg-icons';
 import { RouterLink } from "@angular/router";
+import { ThemeService } from '../services/theme/theme.service';
 
 @Component({
   selector: 'app-header',
@@ -14,16 +15,21 @@ export class HeaderComponent {
   darkIcon = faMoon;
   lightIcon = faSun;
   backIcon = faArrowLeft;
-  openMenuModalBoolean : boolean = false;
-  hasInteracted : boolean = false;
+  openMenuModalBoolean: boolean = false;
+  hasInteracted: boolean = false;
+  private themeService = inject(ThemeService);
 
-  openModal(){
+  toggleTheme(): void {
+    this.themeService.toggleTheme();
+  }
+
+  openModal() {
     this.openMenuModalBoolean = !this.openMenuModalBoolean;
     this.hasInteracted = true;
-    
-    if(this.openMenuModalBoolean){
+
+    if (this.openMenuModalBoolean) {
       document.body.style.overflow = 'hidden';
-    }else{
+    } else {
       document.body.style.overflow = '';
     }
   }
